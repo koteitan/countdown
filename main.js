1c77977f830ba8ecd5181dcb707dc3fa4fd08dd0
koteitan <ukaibutton@mac.com>
2024-04-19 23:10:43 +0900

    stop at 0

--- main.js
+++ main.js
@@ -14,7 +14,10 @@ window.onload = function() {
     // target_date = new Date("2021-12-31T23:59:59");
   }
   if(urlsp.has("title")) {
-    id2e("cdheadline").innerHTML = urlsp.get("title");
+    var str = urlsp.get("title");
+    // sanitize the string
+    str = str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    id2e("cdheadline").innerHTML = str;
   }
   update_countdown();
   setInterval(update_countdown, 500);
@@ -27,12 +30,21 @@ var update_countdown = function() {
   // get the difference in time
   var difference = target_date - current_date;
   
+  //detect minus
+  if(difference < 0) {
+    id2e("cdd").innerHTML = "0";
+    id2e("cdh").innerHTML = "0";
+    id2e("cdm").innerHTML = "0";
+    id2e("cds").innerHTML = "0";
+    return;
+  }
+
   // set the time for the countdown
   var seconds = Math.floor((difference / 1000) % 60);
   var minutes = Math.floor((difference / 1000 / 60) % 60);
   var hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
   var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  
+ 
   // set the display
   id2e("cdd").innerHTML = days;
   id2e("cdh").innerHTML = hours;
